54703a5f65c2a363cf195950938075535194b4a7
Matias Gonzalez <mgonzalez@temperies.com>
2019-07-24 16:12:40 -0300

    navbar added

---
+++ react-test/src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProjectList = () =>{
+    return(
+        <div className="project-list section">
+            
+            <div className="card z-depth-0 proyecy summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"></span>
+                    <p>Some testing posting shit</p>
+                    <p className="grey-text">3rd month and bla bla</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 proyecy summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"></span>
+                    <p>Some testing posting shit</p>
+                    <p className="grey-text">3rd month and bla bla</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 proyecy summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title"></span>
+                    <p>Some testing posting shit</p>
+                    <p className="grey-text">3rd month and bla bla</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
